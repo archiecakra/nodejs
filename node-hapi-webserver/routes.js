6e985d805c468cce636abcbb1a366c4bbf4190ce
titit <archiecakra1@gmail.com>
2021-07-22 19:34:38 +0700

    hapi basic end

--- node-hapi-webserver/routes.js
+++ node-hapi-webserver/routes.js
@@ -7,10 +7,59 @@ const routes = [
     },
   },
   {
+    method: '*',
+    path: '/',
+    handler: (request, h) => {
+      return 'Halaman tidak dapat diakses dengan method tersebut';
+    },
+  },
+  {
     method: 'GET',
     path: '/about',
     handler: (request, h) => {
       return 'About page';
+    },
+  },
+  {
+    method: '*',
+    path: '/about',
+    handler: (request, h) => {
+      return 'Halaman tidak dapat diakses dengan method tersebut';
+    },
+  },
+  {
+    method: 'GET',
+    path: '/hello/{name?}',
+    handler: (request, h) => {
+      const { name = "Stranger" } = request.params;
+      const { lang } = request.query;
+
+      if (lang == 'id') {
+        return `Halooo, selamat datang ${name}`;
+      } else {
+        return `Hellooo, welcome ${name}`;
+      }
+
+    },
+  },
+  {
+    method: 'POST',
+    path: '/login',
+    handler: (request, h) => {
+      const {username, password} = request.payload;
+
+      if (password == 123) {
+        return `Welcome ${username}`;
+      } else {
+        return 'Unauthorized';
+      }
+    }
+  },
+  {
+    method: '*',
+    path: '/{any*}',
+    handler: (request, h) => {
+      return 'Halaman tidak ditemukan';
     },
   },
 ];
