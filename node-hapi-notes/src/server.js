5f07696d9894ab951de9a443eb8d049fdd3b6f0b
titit <archiecakra1@gmail.com>
2021-07-22 22:08:24 +0700

    saving notes

--- node-hapi-notes/src/server.js
+++ node-hapi-notes/src/server.js
@@ -1,1 +1,14 @@
-console.log('test kita akan membuat server');
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 8080,
+    host: 'localhost',
+  });
+
+  await server.start();
+  console.log(`Server mlaku nang ${server.info.uri}`);
+}
+
+init();
