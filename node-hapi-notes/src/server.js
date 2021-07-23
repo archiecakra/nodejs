8696c739b026bd67f907634554c9c6e11b5720ba
sari <archiecakra@kominfo.surabaya.go.id>
2021-07-23 08:35:49 +0700

    mengubah catatan

--- node-hapi-notes/src/server.js
+++ node-hapi-notes/src/server.js
@@ -5,7 +5,14 @@ const init = async () => {
   const server = Hapi.server({
     port: 8080,
     host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
   });
+
+  server.route(routes);
 
   await server.start();
   console.log(`Server mlaku nang ${server.info.uri}`);
